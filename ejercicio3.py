31da2de25cee5675008053428836bdba6a5e4082
ElenaCervera <elena.cerveralopez@usp.ceu.es>
2022-10-02 20:03:58 +0200

    Update ejercicio3.py

--- ejercicio3.py
+++ ejercicio3.py
@@ -5,3 +5,15 @@ lista_1 = ["h",'o','l','a',' ', 'm','u','n','d','o']
 
 lista_2 = ["h",'o','l','a',' ', 'l','u','n','a']
 
+def elementos_repetidos(lista_a, lista_b):
+    lista_elementos_repetidos=[]
+    for i in lista_a:
+        for j in lista_b:
+            if i == j:
+                lista_elementos_repetidos.append(i)
+    print(lista_elementos_repetidos)   
+elementos_repetidos(lista_1, lista_2)    
+
+
+      
+
