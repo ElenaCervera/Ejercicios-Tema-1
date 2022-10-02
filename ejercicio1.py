1bc7b544144372ab304d576d449c5f2a8b5f1ae4
ElenaCervera <elena.cerveralopez@usp.ceu.es>
2022-10-02 16:40:42 +0200

    commit 1

---
+++ ejercicio1.py
@@ -0,0 +1,12 @@
+#Al realizar una consulta en un registro hemos obtenido una cadena de texto corrupta al revés. Al parecer contiene el nombre de un alumno
+# y la nota de un exámen. ¿Cómo podríamos formatear la cadena y conseguir una estructura como la siguiente?
+
+#Nombre Apellido ha sacado un Nota de nota.
+
+#Ayuda
+
+#Para voltear una cadena rápidamente utilizando slicing podemos utilizar un tercer índice -1: cadena[::-1]
+
+cadena = "zeréP nauJ,01"
+voltear_cadena=cadena[::-1]
+print(voltear_cadena[3:], "ha sacado un", voltear_cadena[:2], "de nota.")
